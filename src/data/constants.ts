d4d72a546c4cfd5673f7357facfc579f9744a771
Fabrizio De Vincenzi <hello@fabriziodevincenzi.com>
2023-07-21 23:55:38 +0200

    New course

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -15,8 +15,8 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
+	github: "https://github.com/fabriziodevincenzi",
+	twitter: "https://twitter.com/gnoccodimalga",
+	discord: "https://discord.com",
+	email: "hello@fabriziodevincenzi.com",
 };
